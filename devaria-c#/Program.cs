fed85b89945e96db7b2cca0aa82b9c471a6f7548
Peumariano <pedromariano747@gmail.com>
2023-05-30 21:23:55 -0300

    Adicionar arquivos de projeto.

---
+++ devaria-c#/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string nomeCompleto;
+int idade;
+
+Console.WriteLine("Qual o seu nome completo?");
+nomeCompleto = Console.ReadLine().ToString();
+
+Console.WriteLine("Qual a sua idade?");
+idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Olà, " + nomeCompleto + ", sei que tem " +  idade + " anos, seja bem-vindo ao meu primeiro programa C#");
